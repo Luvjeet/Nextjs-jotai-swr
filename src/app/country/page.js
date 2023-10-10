eef4cd492d7a66a05a55027fc0f8a090eb48c103
Luvjeet <luzag1402@gmail.com>
2023-10-10 14:27:37 +0530

    feat: Batman

--- src/app/country/page.js
+++ src/app/country/page.js
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import Country from "./Country";
 
 export default function page() {
+    const a = 12;
 
     return (
         <Suspense fallback={<div>...loading</div>}>
--- src/app/peru/page.js
+++ src/app/peru/page.js
@@ -1,14 +1,14 @@
 "use client";
 import { useAtom } from "jotai";
 import { getCountryData } from "@/store/countryAtom";
+import { useEffect } from "react";
 
 export default function() {
-    const [coutry] = useAtom(getCountryData);
+    const [coutry] = useAtom(getCountryData)
 
     return (
         <>
             <h2>Peru ka data</h2>
-            {JSON.stringify(coutry)}
         </>
     );
 }
